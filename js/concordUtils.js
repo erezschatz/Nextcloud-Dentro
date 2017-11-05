86f48f84fdccd86b3e874a1d651f77d7dc633822
Erez Schatz <erez@etribez.com>
2017-11-05 13:41:41 +0200

    testing file read

--- js/concordUtils.js
+++ js/concordUtils.js
@@ -24,8 +24,7 @@ const appTypeIcons = {
 	"metaWeblogPost": "file-text-alt"
 };
 
-const initialOpmltext =
-	'<?xml version="1.0" encoding="ISO-8859-1"?><opml version="2.0"><head><title>Untitled</title></head><body><outline text="Foo"/></body></opml>';
+const initialOpmltext = $('#opml').val();
 
 const defaultUtilsOutliner = "#outliner";
 
--- templates/content/index.php
+++ templates/content/index.php
@@ -1,3 +1,30 @@
+<div style="display:none" id="opml">
+<?php
+namespace OCA\MyApp\Storage;
+
+class AuthorStorage {
+
+    private $storage;
+
+    public function __construct($storage){
+        $this->storage = $storage;
+    }
+
+    public function getContent() {
+        // check if file exists and read from it if possible
+        try {
+            $file = $this->storage->get('/dentro.opml');
+            if($file instanceof \OCP\Files\File) {
+                return $file->getContent();
+            } else {
+                throw new StorageException('Can not read from folder');
+            }
+        } catch(\OCP\Files\NotFoundException $e) {
+            throw new StorageException('File does not exist');
+        }
+    }
+}
+</div>
 		<div class="divOutlinerContainer">
 			<div id="outliner">
 			</div>
