f09ab5ad7057d798a1b037cd9611c2e2cb6f0e10
Erez Schatz <erez@etribez.com>
2017-11-05 15:56:58 +0200

    CSRF

--- lib/Controller/PageController.php
+++ lib/Controller/PageController.php
@@ -8,7 +8,6 @@ use OCP\AppFramework\Controller;
 
 class PageController extends Controller {
 	private $userId;
-	private $authorStorage;
 
 	public function __construct($AppName, IRequest $request, $UserId){
 		parent::__construct($AppName, $request);
@@ -29,7 +28,17 @@ class PageController extends Controller {
 		return new TemplateResponse('dentro', 'index');  // templates/index.php
 	}
 	
-    public function opml() {
-        return array(opml => $authorStorage->getContent());
+	/**
+	 * CAUTION: the @Stuff turns off security checks; for this page no admin is
+	 *          required and no CSRF check. If you don't know what CSRF is, read
+	 *          it up in the docs or you might create a security hole. This is
+	 *          basically the only required method to add this exemption, don't
+	 *          add it to any other method if you don't exactly know what it does
+	 *
+	 * @NoAdminRequired
+	 * @NoCSRFRequired
+	 */
+    public function opml($storage) {
+        return array(opml => $storage->getContent());
 	}
 }
