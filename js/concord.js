95a4b28664f6b43b62850e3a020e47d03a2dccdb
Erez Schatz <erez@etribez.com>
2018-02-06 10:35:19 +0200

    an IDIOT

--- js/concord.js
+++ js/concord.js
@@ -814,7 +814,7 @@ function ConcordEditor(root, concordInstance) {
                 } else if (tag == "br") {
                     h = h.replace(
                         new REgExp("&lt; */?" + tag + " */?&lt;"),
-                        "<" + br + ">";
+                        "<" + br + ">");
                 } else {
                     h = h.replace(
                         new RegExp(
